d2d6e99cde6ce58fff504fcf63ee693728c816df
sgordon834 <sgordon834@gmail.com>
2017-02-11 15:22:50 -0800

    Store genertor

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
+var storeHours = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
+
 var pikePlace = {
   min: 17,
   max: 88,
   avg: 5.2,
-  cookieArr = [];
+  cookieArr: [],
 
   ranNumb: function(min, max) {
-    return Math.floor (Math.random() * (this.max - this.min + 1) + this.min);
-}
-}
-// cookieArr = [];
-
-// makeCookie = function() {
-//   for(var i = 0; i < 8; i++) {
-//     this.cookieArr[i] = Math.round(this.ranNumb(this.min, this.max) * this.avg);
-//   }
-// }
-// pikePlace.makeCookie();
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+// console.log(pikePlace.min, pikePlace.max);
+// console.log(pikePlace);
+  makeCookie: function() {
+  for(var i = 0; i < storeHours.length; i++) {
+    var hourlyCookies = Math.round(this.avg) * this.ranNumb();
+    pikePlace.cookieArr.push(hourlyCookies);
+  }
+  console.log(pikePlace.cookieArr);
+  }
+};
+pikePlace.makeCookie();
