896bb4ae31cb3df998954a1dd294dd4c6c453399
sgordon834 <sgordon834@gmail.com>
2017-02-12 14:41:09 -0800

    Tables udpated with render function

--- lab_07/app.js
+++ lab_07/app.js
@@ -1,216 +1,64 @@
-//Global variable for time shop is open
-var storeHours = ['10am:', '11am:', '12pm:', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:'];
+storeHours = ['10am:', '11am:', '12pm:', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:'];
+var pikePlace = new salmonShop ("Pike Place: ", 17, 88, 5.2);
+var seaTac = new salmonShop ("SeaTac: ", 6, 24, 1.2);
+var southCenter = new salmonShop ("Southcenter: ", 11, 38, 1.9);
+var bellevue = new salmonShop ("Bellevue Square: ", 20, 48, 3.3);
+var alki = new salmonShop ("Alki: ", 3, 24, 2.6);
 
-var pikePlace = {
-  min: 17,
-  max: 88,
-  avg: 5.2,
-  total: 0,
-  cookieArr: [],
-  //Generates random number
-  genNumb: function(min, max) {
-    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-  },
-  // console.log(pikePlace.min, pikePlace.max);
-  // function to calculate avg cookies w/ random number
-  cookiePerHr: function() {
-    for (var i = 0; i < storeHours.length; i++) {
-      var hourlyCookies = Math.round(this.avg) * this.genNumb();
-      this.cookieArr.push(hourlyCookies);
-      this.total += hourlyCookies;
+function salmonShop(storeLocation, min, max, avg) {
+  this.storeLocation = storeLocation;
+  this.min = min;
+  this.max = max;
+  this.avg = avg;
+  this.cookieArrHr = [];
+  this.totals = 0;
+};
 
-    }
-    return this.cookieArr;
-    console.log(this.total);
-    // console.log(pikePlace.hourlyCookies);
-  },
-  //Render function to html
-  render: function() {
-    var ul = document.getElementById('pike-place');
-    // console.log(this.cookieArr);
-    var child;
-    for (var i = 0; i < 8; i++) {
-      var li = document.createElement('li');
-      li.textContent = storeHours[i] + ' ' + this.cookieArr[i];
-      ul.appendChild(li);
-    }
-    li.textContent = 'Totals: ' + this.total;
-    ul.appendChild(li);
+salmonShop.prototype.randomNumber = function() {
+  return Math.floor (Math.random() * (this.max - this.min + 1) + this.min);
+};
+
+salmonShop.prototype.salesPerHr = function () {
+  for (var i = 0; i< storeHours.length; i++) {
+  var ranNumb = Math.round(this.randomNumber() * this.avg);
+  this.cookieArrHr.push(ranNumb);
+  this.totals += ranNumb;
   }
 };
-pikePlace.genNumb();
-pikePlace.cookiePerHr();
+
+var table = document.getElementById('cookieData');
+var createRowElement = document.createElement('tr');
+  table.appendChild(createRowElement);
+function genTable(){
+var store = document.createElement('th');
+  store.textContent = 'Store Name:';
+  createRowElement.appendChild(store);
+
+for (var i = 0; i < storeHours.length; i++) {
+  var storeHrs = document.createElement('th');
+  storeHrs.textContent = storeHours[i];
+  createRowElement.appendChild(storeHrs);
+};
+var storeTot = document.createElement('th');
+  storeTot.textContent = 'Total : ';
+  createRowElement.appendChild(storeTot);
+};
+  genTable();
+
+  salmonShop.prototype.render = function() {
+    this.salesPerHr();
+  var storeName = document.createElement('tr')
+  storeName.textContent = this.storeLocation;
+  table.appendChild(storeName);
+
+  for (var i = 0; i < storeHours.length; i++) {
+  var dataElement = document.createElement('td');
+  dataElement.textContent = this.cookieArrHr[i];
+  storeName.appendChild(dataElement);
+  };
+  var cookieTot = document.createElement('td');
+  cookieTot.textContent = this.totals;
+  storeName.appendChild(cookieTot);
+  };
+  bellevue.render();
 pikePlace.render();
-// pikePlace.cookiePerHr();
-
-//Second shop
-var seaTac = {
-  min: 6,
-  max: 24,
-  avg: 1.2,
-  total: 0,
-  cookieArr: [],
-  //Generates random number
-  genNumb: function(min, max) {
-    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-  },
-  // console.log(pikePlace.min, pikePlace.max);
-  // function to calculate avg cookies w/ random number
-  cookiePerHr: function() {
-    for (var i = 0; i < storeHours.length; i++) {
-      var hourlyCookies = Math.round(this.avg) * this.genNumb();
-      this.cookieArr.push(hourlyCookies);
-      this.total += hourlyCookies;
-
-    }
-    return this.cookieArr;
-    console.log(this.total);
-    // console.log(pikePlace.hourlyCookies);
-  },
-  //Render function to html
-  render: function() {
-    var ul = document.getElementById('seaTac');
-    // console.log(this.cookieArr);
-    var child;
-    for (var i = 0; i < 8; i++) {
-      var li = document.createElement('li');
-      li.textContent = storeHours[i] + ' ' + this.cookieArr[i];
-      ul.appendChild(li);
-    }
-    li.textContent = 'Totals: ' + this.total;
-    ul.appendChild(li);
-  }
-};
-seaTac.genNumb();
-seaTac.cookiePerHr();
-seaTac.render();
-// seaTac.cookiePerHr();
-
-// Third Shop
-var southCenter = {
-  min: 11,
-  max: 38,
-  avg: 1.9,
-  total: 0,
-  cookieArr: [],
-  //Generates random number
-  genNumb: function(min, max) {
-    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-  },
-  // console.log(southCenter.min, pikePlace.max);
-  // function to calculate avg cookies w/ random number
-  cookiePerHr: function() {
-    for (var i = 0; i < storeHours.length; i++) {
-      var hourlyCookies = Math.round(this.avg) * this.genNumb();
-      this.cookieArr.push(hourlyCookies);
-      this.total += hourlyCookies;
-
-    }
-    return this.cookieArr;
-    console.log(this.total);
-    // console.log(southCenter.hourlyCookies);
-  },
-  //Render function to html
-  render: function() {
-    var ul = document.getElementById('south-center');
-    // console.log(this.cookieArr);
-    var child;
-    for (var i = 0; i < 8; i++) {
-      var li = document.createElement('li');
-      li.textContent = storeHours[i] + ' ' + this.cookieArr[i];
-      ul.appendChild(li);
-    }
-    li.textContent = 'Totals: ' + this.total;
-    ul.appendChild(li);
-  }
-};
-southCenter.genNumb();
-southCenter.cookiePerHr();
-southCenter.render();
-// southCenter.cookiePerHr();
-
-// Fourth Shop
-var bellevue = {
-  min: 20,
-  max: 48,
-  avg: 3.3,
-  total: 0,
-  cookieArr: [],
-  //Generates random number
-  genNumb: function(min, max) {
-    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-  },
-  // console.log(bellevue.min, pikePlace.max);
-  // function to calculate avg cookies w/ random number
-  cookiePerHr: function() {
-    for (var i = 0; i < storeHours.length; i++) {
-      var hourlyCookies = Math.round(this.avg) * this.genNumb();
-      this.cookieArr.push(hourlyCookies);
-      this.total += hourlyCookies;
-
-    }
-    return this.cookieArr;
-    console.log(this.total);
-    // console.log(bellevue.hourlyCookies);
-  },
-  //Render function to html
-  render: function() {
-    var ul = document.getElementById('bellevue');
-    // console.log(this.cookieArr);
-    var child;
-    for (var i = 0; i < 8; i++) {
-      var li = document.createElement('li');
-      li.textContent = storeHours[i] + ' ' + this.cookieArr[i];
-      ul.appendChild(li);
-    }
-    li.textContent = 'Totals: ' + this.total;
-    ul.appendChild(li);
-  }
-};
-bellevue.genNumb();
-bellevue.cookiePerHr();
-bellevue.render();
-// bellevue.cookiePerHr();
-
-// Fifth Shop
-var alki = {
-  min: 3,
-  max: 24,
-  avg: 2.6,
-  total: 0,
-  cookieArr: [],
-  //Generates random number
-  genNumb: function(min, max) {
-    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-  },
-  // console.log(alki.min, pikePlace.max);
-  // function to calculate avg cookies w/ random number
-  cookiePerHr: function() {
-    for (var i = 0; i < storeHours.length; i++) {
-      var hourlyCookies = Math.round(this.avg) * this.genNumb();
-      this.cookieArr.push(hourlyCookies);
-      this.total += hourlyCookies;
-
-    }
-    return this.cookieArr;
-    console.log(this.total);
-    // console.log(alki.hourlyCookies);
-  },
-  //Render function to html
-  render: function() {
-    var ul = document.getElementById('alki');
-    // console.log(this.cookieArr);
-    var child;
-    for (var i = 0; i < 8; i++) {
-      var li = document.createElement('li');
-      li.textContent = storeHours[i] + ' ' + this.cookieArr[i];
-      ul.appendChild(li);
-    }
-    li.textContent = 'Totals: ' + this.total;
-    ul.appendChild(li);
-  }
-};
-alki.genNumb();
-alki.cookiePerHr();
-alki.render();
-// alki.cookiePerHr();
